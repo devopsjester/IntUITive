bed64b2f6e96b8f3608646c5e109a770efa53a3d
stoneass <stoneas@gmail.como>
2015-06-17 11:21:52 -0400

    Add Intuitive for Selenium

---
+++ IntUITive.Selenium.Tests/IntuitivelyTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IntUITive.Selenium.Tests
+{
+    [TestFixture]
+    public class IntuitivelyTests
+    {
+        [Test]
+        public void Find_WithNoTerm_Fails()
+        {
+            var intuitively = new Intuitively();
+
+            Assert.That(() => intuitively.Find(null), Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Find_WithEmptyTerm_Fails()
+        {
+
+        }
+    }
+}
+
---
+++ IntUITive.Selenium/Intuitively.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace IntUITive.Selenium
+{
+    public class Intuitively
+    {
+        public IWebElement Find(string term)
+        {
+            throw new ArgumentNullException("term", "parameter cannot be empty or null.");
+        }
+    }
+}
