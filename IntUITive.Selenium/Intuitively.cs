58fb574378d9c49bb435b20ea0992658df6e8c94
Assaf Stone <stoneas@gmail.com>
2015-06-18 09:45:31 -0400

    Refactor - Intuitively

--- IntUITive.Selenium/Intuitively.cs
+++ IntUITive.Selenium/Intuitively.cs
@@ -8,41 +8,39 @@ namespace IntUITive.Selenium
 {
     public class Intuitively
     {
-        private readonly IWebDriver _driver;
         private readonly ReadOnlyCollection<IWebElement> _allElementsOnPage;
 
         public Intuitively(IWebDriver driver)
         {
-            _driver = driver;
-            _allElementsOnPage = _driver.FindElements(By.XPath("//*"));
+            _allElementsOnPage = driver.FindElements(By.XPath("//*"));
         }
 
         public IWebElement Find(string term)
         {
-            if (term == null)
-            {
-                throw new ArgumentNullException("term", "parameter cannot be empty or null.");
-            }
-            if (term == string.Empty)
+            if (term.IsNullOrWhiteSpace())
             {
                 throw new ArgumentException("parameter cannot be empty or null.", "term");
             }
 
-            var foundElement = TryFindById(term);
+            var foundElement = FindElementById(term);
 
             return foundElement;
         }
 
-        private IWebElement TryFindById(string term)
+        private IWebElement FindElementByAttribute(string attributeName, string term)
         {
             var pattern = string.Format("{0}", term);
             var possibleElements = from e in _allElementsOnPage
-                                   let elementAttribute = e.GetAttribute("id")
-                                   where Regex.IsMatch(elementAttribute, pattern, RegexOptions.IgnoreCase)
-                                   select e;
+                let elementAttribute = e.GetAttribute(attributeName)
+                where Regex.IsMatch(elementAttribute, pattern, RegexOptions.IgnoreCase)
+                select e;
 
             return possibleElements.FirstOrDefault();
         }
-    }
 
+        private IWebElement FindElementById(string term)
+        {
+            return FindElementByAttribute("id", term);
+        }
+    }
 }
---
+++ IntUITive.Selenium/MiscHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace IntUITive.Selenium
+{
+    public static class MiscHelperExtensions
+    {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string s)
+        {
+            return string.IsNullOrWhiteSpace(s);
+        }
+    }
+}
